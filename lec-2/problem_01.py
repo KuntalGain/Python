f7b606f4742b6f83edc8925a1b210677e3c97bf4
Kuntal Gain <101191826+KuntalGain@users.noreply.github.com>
2022-12-05 18:47:00 +0530

    Solved

--- lec-2/problem_01.py
+++ lec-2/problem_01.py
@@ -2,14 +2,12 @@
 # and s_angry to indicate if each is angry. You are in trouble if 
 # both of them are angry or no one of them is angry.
 
-j_angry = bool(input())
-s_angry = bool(input())
+x = input()
+y = input()
 
-if j_angry == True and s_angry == False:
-    print("You are in Troble")
-elif j_angry == False and s_angry == True:
-    print("You are in Troble")
-elif j_angry == True and s_angry == True:
-    print("You are in Troble")
-elif j_angry == False and s_angry == False:
-    print("You are Safe")
+if x == "true" and y == "true":
+    print("I am in Trouble")
+elif x == "true" or y == "true":
+    print("I am in Trouble")
+else:
+    print("I am Saved")
